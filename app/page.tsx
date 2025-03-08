91d85f12757124efdf365b59c02dbb5cefc6ffd7
Aditya <singhaditya5711@gmail.com>
2025-03-08 12:35:45 +0530

    feat: added bento grid

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ const Home = () => {
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
-        {/* <Grid /> */}
+        <Grid />
         <Experience />
         <RecentProjects />
         <Skills />
